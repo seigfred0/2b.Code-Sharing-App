4f6bc2c206985bbf36841bf910f0de5590490ef7
seigfred0 <saysonseigfred@gmail.com>
2024-07-19 18:10:02 +0800

    backend update

--- backend/index.js
+++ backend/index.js
@@ -31,55 +31,53 @@ app.get('/api', (req, res) => {
     res.json({ hello: 'hello'})
 })
 
-app.get('/api/hello', (req, res) => {
-    res.json({ hello: 'hello'})
+// app.get('/api/hello', (req, res) => {
+//     res.json({ hello: 'hello'})
+// })
+
+app.get('/api/:uniqueId',  async (req, res) => {
+    const { uniqueId } = req.params;
+    const findCode = db.find(code => code.uniqueId === uniqueId)
+    const data = await Database.getCode(uniqueId)
+
+    res.json({ uniqueId })
+    if (data) {
+        res.json({ code: data.code,
+            language: data.language
+        })
+    } else {
+        res.status(404).json({ error: 'Code not found' })
+    }
 })
 
-// app.get('/api/:uniqueId',  (req, res) => {
-    // const { uniqueId } = req.params;
-    // const findCode = db.find(code => code.uniqueId === uniqueId)
-    // const data = await Database.getCode(uniqueId)
 
-    // res.json({ uniqueId })
-    // if (data) {
-    //     res.json({ code: data.code,
-    //         language: data.language
-    //     })
-    // } else {
-    //     res.status(404).json({ error: 'Code not found' })
-    // }
-
-// })
-
-
-// app.put('/api/code/:uniqueId', async (req, res) => {
-//     try {
-//         const { uniqueId }  = req.params;
-//         const { code, language } = req.body;
+app.put('/api/code/:uniqueId', async (req, res) => {
+    try {
+        const { uniqueId }  = req.params;
+        const { code, language } = req.body;
         
 
-//         const result = await Database.updateCode(uniqueId, code, language);
+        const result = await Database.updateCode(uniqueId, code, language);
 
-//         console.log('Updated')
-//         console.log(uniqueId, code)
+        console.log('Updated')
+        console.log(uniqueId, code)
         
-//         res.json({ message: 'updating'})
-//     } catch (error) {
-//         console.error(error)
-//     }
-// })
+        res.json({ message: 'updating'})
+    } catch (error) {
+        console.error(error)
+    }
+})
 
-// app.post('/api/share', (req, res) => {
-//     const { code, language } = req.body;
-//     const uniqueId = generateId.generate();
-//     const newCode = {uniqueId, language, code};
-//     Database.saveCode(newCode);
+app.post('/api/share', (req, res) => {
+    const { code, language } = req.body;
+    const uniqueId = generateId.generate();
+    const newCode = {uniqueId, language, code};
+    Database.saveCode(newCode);
 
 
-//     res.json({ uniqueId })
-// });
+    res.json({ uniqueId })
+});
 
-// delete this
 app.listen(port, () => {
     console.log('it is running in port: 3000')
 })
